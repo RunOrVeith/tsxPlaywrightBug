3fe660d78f0697d36890044a7f03dc00e74d9e74
Veith Röthlingshöfer <veith.roethlingshoefer@octomind.dev>
2024-07-05 12:09:52 +0200

    Minimal reproducing example

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { chromium } from "playwright";
+
+declare global {
+  interface Window {
+    example: {foo: () => number};
+  }
+}
+
+const main = async (): Promise<void> => {
+    const browser = await chromium.launch();  
+    const context = await browser.newContext();
+    await context.addInitScript(() => {
+        window.example = { foo: () => 5}
+    })
+    const page = await context.newPage();
+    console.log(await page.evaluate(() => window.example.foo()))
+};
+
+if (require.main === module) {
+    main()
+}
